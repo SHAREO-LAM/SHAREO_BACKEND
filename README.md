4bad25e4a2b3e5abfaf4d306898aa5911792095a
Alexandre CAROL <sasandre10@gmail.com>
2025-10-22 16:21:20 +0200

    first commit re-using POC

---
+++ README.md
@@ -0,0 +1,38 @@
+# projet_annuel_poc
+
+Ce projet est une API backend développée avec NestJS, qui met en place une gestion d'utilisateurs avec une mise à jour partielle des données en utilisant TypeORM (natif nestJS).
+
+## Technologies utilisées
+
+- **NestJS** : Framework backend Node.js progressif
+- **TypeORM** : ORM pour TypeScript et JavaScript
+- **Swagger** : Documentation d'API interactive
+
+## Fonctionnalités
+
+- Création d'utilisateurs
+- Consultation de tous les utilisateurs ou d'un utilisateur spécifique
+- Mise à jour d'utilisateurs
+- Mise à jour partielle d'utilisateurs
+- Suppression d'utilisateurs
+
+## Installation et utilisation
+
+```bash
+git clone https://github.com/Zastial/PA_POC_Backend
+
+cd PA_POC_Backend
+
+npm i
+npm run start
+```
+
+## Accès
+
+```bash
+# API
+http://localhost:3000
+
+# Documentation Swagger
+http://localhost:3000/api
+```
---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: '*',
+  });
+  app.setGlobalPrefix('api');
+
+  // Global validation configuration
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Removes properties that are not defined in DTOs
+      transform: true, // Automatically transforms payloads to DTO instances/types
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('User API')
+    .setDescription('API de gestion des utilisateurs')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+
+  const url = await app.getUrl();
+  console.log(`Application is running on: ${url}/api`);
+  console.log(`Swagger documentation is available at: ${url}/docs`);
+}
+
+void bootstrap(); // NOSONAR
