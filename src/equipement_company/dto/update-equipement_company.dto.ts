145010d7f652ba3844afe340dce9967eec78fc86
MathisMchd <michenaudmathis@gmail.com>
2026-02-21 15:04:53 +0100

    dashbord et homepage

--- src/equipement_company/dto/update-equipement_company.dto.ts
+++ src/equipement_company/dto/update-equipement_company.dto.ts
@@ -1,6 +1,11 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateEquipementCompanyDto } from './create-equipement_company.dto';
+import { IsString } from 'class-validator';
 
 export class UpdateEquipementCompanyDto extends PartialType(
   CreateEquipementCompanyDto,
-) {}
+) {
+    @ApiProperty({ description: 'Id de l’équipement' })
+    @IsString()
+    id: string;
+}
